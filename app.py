64216139df182872475027e4e7eccafa245aff3b
apipoledeng <zizugaming13@gmail.com>
2023-06-20 01:48:59 +0800

    CRUD surat pemberitahuan

--- app.py
+++ app.py
@@ -351,19 +351,122 @@ def surat_keluar_update(id):
 
 @app.route("/list-surat-pemberitahuan")
 def list_surat_pemberitahuan():
-    return render_template('listpemberitahuan.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(
+            token_receive, SECRET_KEY,algorithms=['HS256']
+        )
+        user_info=db.users.find_one({'username':payload.get('id')})
+        list_surat = db.letters.find({'kategori':'SP'})
+        jumlah_surat = db.letters.count_documents({'kategori':'SP'})
+        return render_template('listsuratpemberitahuan.html', list_surat=list_surat, jumlah_surat=jumlah_surat,user_info=user_info)
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for('dashboard'))
+    
 
-@app.route("/list-surat-pengumuman")
-def list_surat_pengumuman():
-    return render_template('listpengumuman.html')
+@app.route("/add-surat-pemberitahuan")
+def add_surat_pemberitahuan():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(
+            token_receive, SECRET_KEY,algorithms=['HS256']
+        )
+        return render_template('addsuratpemberitahuan.html')
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for('dashboard'))
+    
+@app.route("/add-surat-pemberitahuan-save", methods=['POST'])
+def add_surat_pemberitahuan_save():
+    nomor_surat_receiver = request.form.get('nomor_surat')
+    kategori_receiver = request.form.get('kategori')
+    pengirim_receiver = request.form.get('pengirim')
+    tanggal_receiver = request.form.get('tanggal')
+    tujuan_receiver = request.form['tujuan']
+    perihal_receiver = request.form['perihal']
+    lampiran_receiver = request.files['lampiran']
+    keterangan_receiver = request.form.get('keterangan')
 
-@app.route("/editsurat")
-def edit_surat():
-    return render_template('editsurat.html')
 
-@app.route("/edit-profile")
-def edit_profile():
-    return render_template('profil-modify1.html')
+    #simpan file di tmp/foo
+    lampiran_receiver.save('/tmp/foo')
+    #akses dan cek ukuran file pada tmp/foo dir
+    file_size = os.stat('/tmp/foo').st_size
+    extension_surat = lampiran_receiver.filename.split(".")[-1]
+    #cek ukuan file
+    if file_size <= 5*1000*1000:
+        #cek extensi
+        if extension_surat not in ALLOWED_FILE:
+            flash('The file must be .docs, .doc, and .pdf ', 'error')
+            return redirect(url_for('list_surat_pemberitahuan'))
+        #simpan surat
+        tanggal_to_datetime = datetime.strptime(tanggal_receiver, "%Y-%m-%d")
+        datetime_to_tanggal = tanggal_to_datetime.strftime('%d-%m-%Y')
+        print(tanggal_to_datetime)
+        print(datetime_to_tanggal)
+        surat = {
+            'nomor_surat':f'{kategori_receiver}/{nomor_surat_receiver}',
+            'kategori': kategori_receiver,
+            'tanggal':datetime_to_tanggal,
+            'tujuan': tujuan_receiver,
+            'perihal': perihal_receiver,
+            'pengirim':pengirim_receiver,
+            'keterangan':keterangan_receiver
+        }
+        print(lampiran_receiver)
+        filename = secure_filename(lampiran_receiver.filename) #secure file
+        file_path = f"letters/{kategori_receiver}_{nomor_surat_receiver}_{datetime_to_tanggal}_{filename}" #membuat path baru
+        lampiran_receiver.save(os.path.join(BASE_DIR+'/lexiarchiver/static/', file_path)) #menyimpan file
+        surat["lampiran"] = file_path #menambah key value
+        db.letters.insert_one(surat)
+        flash('The letter has been successfully saved.', 'success')
+        return redirect(url_for('list_surat_pemberitahuan'))
+    else:
+        flash('The file size exceeds 5 Mb.', 'error')
+        return redirect(url_for('list_surat_pemberitahuan'))
+
+@app.route("/surat-pemberitahuan/delete/<id>", methods=['GET'])
+def surat_pemberitahuan_delete(id):
+    db.letters.delete_one({'_id':ObjectId(id)})
+    flash('The letter has been successfully deleted.','success')
+    return redirect(url_for('list_surat_pemberitahuan'))
+
+
+@app.route("/surat-pemberitahuan/update/<id>", methods=['GET','POST'])
+def surat_pemberitahuan_update(id):
+    obj = db.letters.find_one({'_id':ObjectId(id)})
+    print(obj)
+    if request.method == 'POST':
+        print("Ngepost")
+        tanggal_to_datetime = datetime.strptime(request.form['tanggal'], "%Y-%m-%d")
+        datetime_to_tanggal = tanggal_to_datetime.strftime('%d-%m-%Y')
+        lampiran_receiver =request.files['lampiran']
+        if request.files != '':
+            filename = secure_filename(lampiran_receiver.filename) #secure file
+            file_path = f"letters/{request.form['kategori']}_{request.form['nomor_surat']}_{datetime_to_tanggal}_{filename}" #membuat path baru
+            print(file_path)
+            lampiran_receiver.save(os.path.join(BASE_DIR+'/lexiarchiver/static/', file_path)) #menyimpan file
+        else:
+            flash('File suratnya nggak ada','error')
+            return redirect(url_for('list_surat_pemberitahuan'))
+        data = {
+            'nomor_surat':f"SP/{request.form['nomor_surat']}",
+            'kategori':request.form['kategori'],
+            'tanggal':datetime_to_tanggal,
+            'tujuan':request.form['tujuan'],
+            'perihal':request.form['perihal'],
+            'pengirim':request.form['pengirim'],
+            'keterangan':request.form['keterangan'],
+            'lampiran':file_path
+        }
+        db.letters.update_one({'_id':ObjectId(id)},{'$set': data})
+        flash('The letter has been successfully edited.','success')
+        return redirect(url_for('list_surat_pemberitahuan'))
+    obj['nomor_surat']=obj['nomor_surat'].replace("SP/",'')
+    date_object = datetime.strptime(obj['tanggal'], '%d-%m-%Y')
+    new_date_string = date_object.strftime('%Y-%m-%d')
+    obj['tanggal'] = new_date_string
+    print(obj['kategori'])
+    return render_template('editsuratpemberitahuan.html', surat=obj, id=id)
 
 
 if __name__ == '__main__':
